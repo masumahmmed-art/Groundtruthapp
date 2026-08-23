89d79809cd669b022434f9736cf381382b2e804a
masumahmmed-art <masum.ahmmed@gmail.com>
2026-08-23 21:57:02 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "groundtruthestimator.com",
+        "www.groundtruthestimator.com",
+        "groundtruthapp.vercel.app",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
